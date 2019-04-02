9cafd0c44c2a42699d333450aa67a7adf7244e5f
apinodominguez <apinodominguez@nautilus3.danielcastelao.org>
2019-04-02 12:00:12 +0200

    Boletin 25.

---
+++ src/boletin25/Boletin25.java
@@ -0,0 +1,13 @@
+package boletin25;
+
+public class Boletin25 {
+
+    public static void main(String[] args) {
+
+        Ventana1 ven1 = new Ventana1();
+        ven1.setVisible(true);
+          
+        
+    }
+    
+}
